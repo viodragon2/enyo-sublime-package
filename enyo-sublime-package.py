81bce8fa82d7c11729cd0f033b3dbffb78efc2dc
Stephen Choi <stephen.choi@lge.com>
2015-12-22 17:48:06 -0800

    Fix settings file not loading at startup in ST3

--- enyo-sublime-package.py
+++ enyo-sublime-package.py
@@ -1,9 +1,17 @@
 import sublime, sublime_plugin
 
-class enyoJSAttributeCompletion(sublime_plugin.EventListener):
-
-	def __init__(self):
+class EnyoJS:
+	def init(self):
 		self.settings = sublime.load_settings('enyo-completions.sublime-settings')
+
+	def completions(self):
+		core_compl = [tuple(attr) for attr in self.settings.get('completions_list')]
+		extended_compl = [tuple(attr) for attr in self.settings.get('extended_completions_list')]
+		extended_compl += core_compl
+		return (extended_compl, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+
+class enyoJSAttributeCompletion(sublime_plugin.EventListener):
+	global enyo
 
 	def on_query_completions(self, view, prefix, locations):
 		# Only trigger within JS
@@ -11,10 +19,9 @@ class enyoJSAttributeCompletion(sublime_plugin.EventListener):
 				'source.js - string.quoted - comment - meta.brace.square'):
 			return []
 
-		return self.completions()
+		return enyo.completions()
 
-	def completions(self):
-		core_compl = [tuple(attr) for attr in self.settings.get('completions_list')]
-		extended_compl = [tuple(attr) for attr in self.settings.get('extended_completions_list')]
-		extended_compl += core_compl
-		return (extended_compl, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+enyo = EnyoJS()
+
+def plugin_loaded():
+	enyo.init()
